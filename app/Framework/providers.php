d2d0eb7843f5093743e0a1070eba8e818f897e38
Dennis Lindeboom <dlindeboom19@outlook.com>
2024-07-01 20:54:26 +0200

    IMP structure of code and added .env.warden - part 2

---
+++ app/Framework/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \App\Framework\Providers\AppServiceProvider::class,
+    \App\Framework\Providers\ViewServiceProvider::class,
+    \App\Framework\Providers\MigrationServiceProvider::class,
+];
