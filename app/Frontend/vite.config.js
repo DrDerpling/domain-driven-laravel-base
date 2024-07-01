28ca675adb3e9e7dd9fb1ddd070871d4fd0f9ac0
Dennis Lindeboom <dlindeboom19@outlook.com>
2024-07-01 22:04:00 +0200

    FIX vite.config.js with the new structure

--- app/Frontend/vite.config.js
+++ app/Frontend/vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['app/Domains/Frontend/resources/js/app.js', 'app/Domains/Frontend/resources/css/app.css'],
+            input: ['app/Frontend/js/app.js', 'app/Frontend/css/app.css'],
             refresh: true,
         }),
     ],
